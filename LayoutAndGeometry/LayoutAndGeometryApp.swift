7973637406fbdfca8d68c5cede8326e25c6e64b2
Igor Verkhov <iVerkhov@icoud.com>
2024-02-26 16:08:26 +0300

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Игорь Верхов on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
