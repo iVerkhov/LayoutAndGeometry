cb699e2daeca24f60b3ba3a99b28237a4765eec1
Igor Verkhov <iVerkhov@icoud.com>
2024-02-27 18:36:00 +0300

    New version of the course

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        let opacity = proxy.frame(in: .global).minY / 200
+                        let scale = min(1,
+                                        max(0.5,
+                                            proxy.frame(in: .global).maxY / fullView.frame(in: .global).height)
+                                        )
+                        let scale2 = max(0.5, proxy.frame(in: .global).minY / 400)
+                        let hue = min(1, (proxy.frame(in: .global).maxY / fullView.frame(in: .global).height))
+                        
+                        Text("Row #\(index) : \(opacity.formatted()) : \(scale.formatted()) : \(hue.formatted())")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hue: hue, saturation: 0.5, brightness: 1))
+                            .opacity(opacity)
+                            .scaleEffect(scale)
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
